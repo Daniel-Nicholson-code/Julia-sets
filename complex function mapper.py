491dc9cee9609a2c666fc1bae433ca19b02ba5ea
Daniel Nicholson <danielnicholson239@gmail.com>
2025-08-29 23:57:16 +0100

    Update complex function mapper.py

--- complex function mapper.py
+++ complex function mapper.py
@@ -7,46 +7,47 @@ import time
 
 from PIL import Image
 
-#variables
+# variables
 width = 250
 height = 250
 domain = 3
 
-#keeping track of how long the program took to run
+# keeping track of how long the program took to run
 startTime = time.time()
 
-#the function that is being mapped
+# the function that is being mapped
 def f(posx,posy):
   z = complex(posx,posy)
   z = (2*z - 5) / (3*z - 2)
   return z.real,z.imag,abs(z)
 
-#generating a blank image
+# generating a blank image
 image = Image.new("RGB", (width, height))
 
-#iterating through every pixel of the image
+# iterating through every pixel of the image
 for x in range(width):
   for y in range(height):
 
-    #converts pixel coordinates to cartesian
+    # converts pixel coordinates to cartesian
     posx = 2*x*domain/width - domain
     posy = 2*(height-y)*domain/height - domain
 
-    #runs the function on the pixel coordinates and saves the ouput
+    # runs the function on the pixel coordinates and saves the ouput
     posfx,posfy,mod = f(posx,posy)
 
-    #converts the x,y of the output into a colour
-    #(using precomputed values for sine and cosine)
+    # converts the x,y of the output into a colour
+    # (using precomputed values for sine and cosine)
     mod = 81.1875*mod*0.25
     red = min(int(abs(math.atan2(posfy*0.866-posfx*0.5,posfx*-0.866-posfy*0.5))*mod), 255)
     green = min(int(abs(math.atan2(posfy*-0.866-posfx*0.5,posfx*0.866-posfy*0.5))*mod), 255)
     blue = min(int(abs(math.atan2(posfx,posfy))*mod), 255)
 
-    #fills in the pixel the colour of the output
+    # fills in the pixel the colour of the output
     image.putpixel((x,y),(red,green,blue))
 
-#displaying how long the program took to run
+# displaying how long the program took to run
 print("program finished in",str(round(time.time()-startTime,3))+"s")
 
 #displaying final image
 image.show()
+
