1a5ba6cd49cf22d0f8bcc8e696c720e00a31d125
Daniel Nicholson <danielnicholson239@gmail.com>
2025-09-02 23:02:34 +0100

    Update Mandelbrot generator.py

--- Mandelbrot generator.py
+++ Mandelbrot generator.py
@@ -7,17 +7,17 @@ import time
 
 from PIL import Image
 
-#variables
+# variables
 width = 400
 height = 400
 domain = 1.2
 maxI = 300
 cameraPos = complex(-0.747,0.1)
 
-#keeping track of how long the program took to run
+# keeping track of how long the program took to run
 startTime = time.perf_counter()
 
-#iterates the function and returns how long it took to diverge
+# iterates the function and returns how long it took to diverge
 def f(c):
   z = complex(0,0)
   c = c + cameraPos
@@ -27,26 +27,26 @@ def f(c):
     if mod >= 2 or math.isnan(mod): break
   return i
 
-#function to generate colour gradient
+# function to generate colour gradient
 def interpolate(r1,g1,b1,r2,g2,b2,t):
   red = round((r2-r1)*t + r1)
   green = round((g2-g1)*t + g1)
   blue = round((b2-b1)*t + b1)
   return red,green,blue
 
-#generating a blank image
+# generating a blank image
 image = Image.new("RGB", (width, height))
 
-#iterating through every pixel of the image
+# iterating through every pixel of the image
 for x in range(width):
   for y in range(height):
 
-    #converts pixel coordinates to cartesian
+    # converts pixel coordinates to cartesian
     posx = 2*x*domain/width - domain
     posy = 2*(height-y)*domain/height - domain
 
-    #runs the function on the pixel coordinates and sets the
-    #colour of the pixel to the iterations until it diverges
+    # runs the function on the pixel coordinates and sets the
+    # colour of the pixel to the iterations until it diverges
     i = f(complex(posx,posy))
     if i == maxI-1:
       image.putpixel((x,y),(0,0,0))
@@ -62,8 +62,9 @@ for x in range(width):
         red,green,blue = interpolate(254,221,159,0,0,0,i/0.5)
       image.putpixel((x,y),(red,green,blue))
 
-#displaying how long the program took to run
+# displaying how long the program took to run
 print(f"program finished in {(time.perf_counter()-startTime):.3f}s")
 
 #displaying final image
 image.show()
+
